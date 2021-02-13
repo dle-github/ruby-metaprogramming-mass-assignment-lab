7fa97a1e56a840aad37366ba7a1b236aa8e340d6
derek <derekle.creative@gmail.com>
2021-02-13 03:08:09 -0600

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
-  #your code here
+  def initialize(input)
+    p "initialize [Person] class with hash [#{input}]"
+    input.each do |key, value|
+      p "create attr_accessor for [#{key}]" 
+      self.class.attr_accessor(key)
+      p "sets key [#{key}] with value of [#{value}]" 
+      self.send(("#{key}="), value)
+    end
+  end
 end
